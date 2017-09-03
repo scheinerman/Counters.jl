30c2f961d5159444fa18567ec2a7770801e5c4a9
Ed Scheinerman <ers@jhu.edu>
2017-09-03 10:38:02 -0400

    More loading

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,18 @@ using Counters
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@test 1 == 1
+c = Counter{String}()
+c["alpha"] = 1
+incr!(c,"bravo")
+c["charlie"] += 1
+
+@test c["alpha"] == 1
+@test c["bravo"] == 1
+@test c["charlie"] == 1
+
+c = c+c
+
+@test c["alpha"] == 2
+@test nnz(c) == 3  
+@test sum(c) == 6
